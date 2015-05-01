0a862c48067809cf785995893e1517e2b7bbcf62
David Brough <david.brough.0416@gmail.com>
2015-04-30 20:08:10 -0400

    Restructure files
    Restructure pymks directory so that both MKSHomogenizationModel and
    MKSLocalizationModel both show on the documentation.

---
+++ pymks/mks_models/__init__.py
@@ -0,0 +1,4 @@
+from .mks_homogenization_model import MKSHomogenizationModel
+from .mks_localization_model import MKSLocalizationModel
+
+__all__ = ['MKSLocalizationModel', 'MKSHomogenizationModel']
